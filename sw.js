c364426b14ada45b42117ff799993755fa2726e7
m81-ito-keisuke <larc_bump717kei@yahoo.co.jp>
2026-01-15 00:30:16 +0900

    PWA

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'dear-list-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon.svg'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => cacheName !== CACHE_NAME)
+                    .map((cacheName) => caches.delete(cacheName))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
